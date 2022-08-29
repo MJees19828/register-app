c9ce4400b49918771f39f0f4e15a9bfabd437bff
Alhaasan Abdul-majeed Baanga <maj.baanga60@gmail.com>
2022-08-29 14:41:45 +0000

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React,{useState,useEffect,useContext} from 'react'
+import{link,useNavigate} from 'react-router-dom'
+
+
+function Header() {
+    const navigate =useNavigate();
+    const {success,loading,user} =useContext()
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Header
