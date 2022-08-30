72d1c59e2af86d8554888a0bb011bb8563ed9bce
Alhaasan Abdul-majeed Baanga <maj.baanga60@gmail.com>
2022-08-30 14:55:59 +0000

    first commit

--- src/Screen/context/AuthContext.js
+++ src/Screen/context/AuthContext.js
@@ -10,7 +10,15 @@ function AuthContext() {
   const [error,setError] =useState("null");
   const [loading,setLoading] =useState("false");
   const [user,setUser] =useState("null");
+
   
+  //signuo function
+  const authRegister=async(username,email,password)=>{
+    await fetch(url,{
+      method:"POST"
+
+    })
+  }
   return (
     <div>
 
@@ -18,4 +26,4 @@ function AuthContext() {
   )
 }
 
-export default AuthContext
+export default AuthContextProvider
